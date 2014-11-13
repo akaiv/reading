b04be0aa87fc49026dded82cfd2aa7e40400af39
s10n <a@akaiv.com>
2014-11-13 20:22:02 +0900

    피드: 이미지 제거

--- inc/article.php
+++ inc/article.php
@@ -5,9 +5,6 @@ remove_filter( 'the_excerpt', 'wpautop' );
 /* 본문에 URL 명시(feed) */
 function akaiv_insert_content_footnote($content) {
   if( is_feed() ) :
-    if ( has_post_thumbnail() ) :
-      $content.= '<p class="post-thumbnail">'.get_the_post_thumbnail(null, 'large').'</p>';
-    endif;
     $content.= '<p>원문 바로가기: <a href="'.akaiv_get_url().'">'.akaiv_get_url().'</a></p>';
   endif;
   return $content;
